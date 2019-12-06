3ffe8c03b8892dfec5556581a5ca7640933351e8
Greg Shanks <greg.shanks@gmail.com>
2019-12-06 19:13:21 +0000

    Defined setup test function for CardGame class testing.

--- Static_and_Dynamic_Task_A/specs/card_game_spec.rb
+++ Static_and_Dynamic_Task_A/specs/card_game_spec.rb
@@ -6,7 +6,25 @@ require_relative('../card.rb')
 class TestCardGame < Minitest::Test
 
   def setup()
-    
+    @card1 = Card.new("Spades", 1)
+    @card2 = Card.new("Hearts", 8)
+    @card3 = Card.new("Clubs", 6)
+  end
+
+  def test_check_for_ace_true()
+
+  end
+
+  def test_check_for_ace_false()
+
+  end
+
+  def test_highest_card()
+
+  end
+
+  def test_cards_total()
+
   end
 
 end
