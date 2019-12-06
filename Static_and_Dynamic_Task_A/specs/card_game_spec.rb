22a961655775eb2df9dff8b731b531725068cb7b
Greg Shanks <greg.shanks@gmail.com>
2019-12-06 19:03:13 +0000

    Initialised spec file for testing and added require_relatives

---
+++ Static_and_Dynamic_Task_A/specs/card_game_spec.rb
@@ -0,0 +1,12 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../card_game.rb')
+require_relative('../card.rb')
+
+class TestCardGame < Minitest::Test
+
+  def setup()
+    
+  end
+
+end
